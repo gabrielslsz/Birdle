3a2426f9326d557a1357f6225c50eb2e60cff9be
Gabriel Sousa <gaabdev42@gmail.com>
2026-06-07 13:45:18 -0300

    Grade 5x5 de quadrados brancos.

--- lib/main.dart
+++ lib/main.dart
@@ -35,16 +35,45 @@ class Tile extends StatelessWidget {
   }
 }
 
+class GamePage extends StatelessWidget {
+  GamePage({super.key});
+  final Game _game = Game();
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        spacing: 5.0,
+        children: [
+          for (final guess in _game.guesses)
+            Row(
+              spacing: 5.0,
+              children: [
+                for (final letter in guess) Tile(letter.char, letter.type),
+              ],
+            ),
+        ],
+      ),
+    );
+  }
+}
+
+
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Tile('A', HitType.hit), // NEW
+        appBar: AppBar(
+          title: const Align(
+            alignment: Alignment.centerLeft,
+            child: Text('Birdle'),
+          ),
         ),
+        body: Center(child: GamePage()), 
       ),
     );
   }
